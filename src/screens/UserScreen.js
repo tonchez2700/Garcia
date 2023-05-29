7879616ee48bf17803f72c926ac398fca3ab0858
LAPTOP-702TL96E\on1xg <al02891135@tecmilenio.mx>
2023-05-29 09:53:13 -0600

    Maquetacion UserScreen

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,65 +1,133 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useContext } from "react";
 import {
-    StyleSheet, View, ScrollView,
-    Text, ActivityIndicator, TextInput
-} from 'react-native';
-import { Context as RegistrationContext } from '../context/RegistrationContext';
-import { general } from '../theme/customTheme';
-import { Icon, Button, Slider } from 'react-native-elements'
-import { useNavigation } from '@react-navigation/native';
-import tw from 'tailwind-react-native-classnames'
-import MenuItem from '../components/MenuItem';
-import moment from 'moment';
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { Context as RegistrationContext } from "../context/RegistrationContext";
+import { Button } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import InputForm from "../components/Forms/InputForm";
 
 const UserScreen = () => {
+  const navigation = useNavigation();
+  const { state, handleInputChange } = useContext(RegistrationContext);
+  const imageUser = {
+    uri: "https://www.fakepersongenerator.com/Face/female/female20161025132706268.jpg",
+  };
 
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <View style={{ alignItems: "center", padding: 20 }}>
+        <Text style={styles.nameHighlight}>Sharon T Lett</Text>
+        <Image style={styles.userImage} source={imageUser} />
+        <Text style={styles.label}>Nombre</Text>
+        <Text style={styles.userInfo}>Sharon T Lett</Text>
+      </View>
+      <View style={{ width: "100%", height: "40%", }}>
+        <Text style={styles.textInput}>Nombre</Text>
+        <InputForm
+          maxLength={50}
+          name="Name"
+          placeholder="Nombre"
+          inputContainerStyle={styles.input}
+        />
+        <Text style={styles.textInput}>Correo</Text>
+        <InputForm
+          maxLength={50}
+          name="correo"
+          placeholder="Correo"
+          inputContainerStyle={styles.input}
+          keyboardType="email-address"
+        />
+        <Text style={styles.textInput}>Contraseña</Text>
+        <InputForm
+          maxLength={15}
+          name="password"
+          inputContainerStyle={styles.input}
+          placeholder="Contraseña"
+          secureTextEntry={true}
+        />
 
-    const navigation = useNavigation();
-    const { state, handleInputChange } = useContext(RegistrationContext);
+        <View
+          style={{
+            width: "100%",
+            height: 50,
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <TouchableOpacity style={styles.saveButton}>
+            <Text style={styles.textNameUser}>Guardar cambios</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
-    // useEffect(() => {
-    //     const unsubscribe = navigation.addListener('blur', () => {
-    //   
-    //     });
-    //     return unsubscribe;
-    // }, [navigation]);
+export default UserScreen;
 
-    const renderContent = () => {
-
-        return (
-            <View style={{ flex: 1, backgroundColor: '#F7F8FAF', padding: 20, }}>
-                <View style={{ flex: 1, backgroundColor: '#FFFFFF', padding: 20, borderWidth: 1, borderColor: '#7F7F7F', borderRadius: 10, alignItems: 'center' }}>
-                    <View style={{ flex: 1, flexDirection: 'column', width: '100%' }}>
-                        <Text>UserScren</Text>
-                    </View>
-                </View >
-            </View >
-
-        );
-    }
-
-    return (
-        !state.fetchingData
-            ?
-            !state.error
-                ?
-                renderContent()
-                :
-                <View style={tw`flex-1 p-5 justify-center items-center`}>
-                    <Text style={tw`text-center text-lg mb-3`}>
-                        {state.message}
-                    </Text>
-                    <Button
-                        containerStyle={{ width: 120 }}
-                        buttonStyle={[{ backgroundColor: '#118ea6' }]}
-                        title="Actualizar"
-                        onPress={() => navigation.navigate("HomeScreen")}
-                    />
-                </View>
-            :
-            <ActivityIndicator size="large" color="#118EA6" style={tw`mt-5`} />
-    )
-}
-export default UserScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#1E0554",
+  },
+  nameHighlight: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  userImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    margin: 20,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  userInfo: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  input: {
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+  textInput: {
+    paddingLeft: 32,
+    fontSize: 14,
+    color: "#fff",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingLeft: 15,
+    borderRadius: 40,
+    borderWidth: 1,
+    borderColor: "#707070B3",
+  },
+  saveButton: {
+    width: "92%",
+    height: 30,
+    backgroundColor: "#629DF6",
+    borderRadius: 30,
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+    // borderColor: "#000", borderWidth: 1
+  },
+});
